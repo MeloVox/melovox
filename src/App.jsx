ed45ceafda912a99a3293a492a33ab7673423014
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-25 14:02:20 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css'
+function App() {
+
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <h1 className="font-bold text-4xl">Melovox Project</h1>
+    </div>
+  )
+}
+
+export default App
