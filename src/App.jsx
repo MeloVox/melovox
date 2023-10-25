bc1caaf98385c7c6e722612bc8e3a1eb04587720
sohkey <thomas.rodriguez38530@gmail.com>
2023-10-25 16:21:56 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import './index.css'
 function App() {
-
   return (
     <div className="h-screen w-full flex justify-center items-center">
       <h1 className="font-bold text-4xl">Melovox Project</h1>
-      <a href="te">te st</a>
+      <a href="">test</a>
     </div>
   )
 }
