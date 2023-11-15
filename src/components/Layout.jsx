1390dfc3f7895a8af0b44b9d65d90c2a2d60df1b
sohkey <sohkey.fetch@gmail.com>
2023-11-15 11:02:44 +0100

    feat: add register

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -11,6 +11,9 @@ const Layout = () => {
           <li>
             <Link to="/login">Login</Link>
           </li>
+          <li>
+            <Link to="/register">Login</Link>
+          </li>
         </ul>
       </nav>
 
---
+++ src/components/Register.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+
+function Register() {
+    const [message, setMessage] = useState('');
+    const [mail, setMail] = useState("");
+    const [password, setPassword] = useState("");
+    const API = "http://127.0.0.1:3333"
+
+    const submitLogin = (mail, password) => {
+        fetch(`${API}/api/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: mail, password }),
+        }).catch((err) => {
+            err.json().then((data) => {
+                const { message } = data
+                setMessage(message)
+            })
+        }).then((reponse) => {
+            reponse.json().then((data) => {
+                const { message } = data
+                setMessage(message)
+            })
+        })
+    }
+
+    return (
+        <div className="h-screen w-full flex justify-center items-center">
+            <div className="flex flex-col space-y-5 m-5 border border-black rounded-md p-5">
+                <h1 className="font-bold text-4xl">Register</h1>
+                <form className="flex flex-col space-y-5" onSubmit={(e) =>{ e.preventDefault(); submitLogin(mail, password); }}>
+
+                    <div className="flex flex-col">
+                        <label htmlFor="">Mail</label>
+                        <input
+                            value={mail}
+                            onChange={(e) => setMail(e.target.value)}
+                            className="border border-black" type="mail" name="" id="" />
+                    </div>
+                    <div className="flex flex-col">
+                        <label htmlFor="">Password</label>
+                        <input
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className="border border-black" type="password" />
+                    </div>
+                    <button className="w-fit border border-black" type="submit">Login</button>
+                    <p>{message}</p>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Register
