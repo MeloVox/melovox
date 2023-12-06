4e49c2ae3495236b129da588aedeee07adb756a8
AmauryRDV <amaury.richarddevesvrotte@gmail.com>
2023-12-06 10:20:24 +0100

    Remove boostrap for Search and Artist

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,18 @@
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import {
-  Container,
-  InputGroup,
-  FormControl,
-  Button,
-  Row,
-  Card,
-  CardBody,
-  CardTitle,
-  CardText,
-  Col,
-  CardImg,
-  CardSubtitle,
-} from 'react-bootstrap'
+// import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+// import {
+//   Container,
+//   InputGroup,
+//   FormControl,
+//   Button,
+//   Row,
+//   Card,
+//   CardBody,
+//   CardTitle,
+//   CardText,
+//   Col,
+//   CardImg,
+//   CardSubtitle,
+// } from 'react-bootstrap'
 import { useState, useEffect } from 'react'
 
 //CLE SPOTIFY CLIENT ID ET CLIENT SECRET
@@ -68,8 +68,8 @@ function App() {
     )
       .then(response => response.json())
       .then(data => data.artists.items)
-
     setArtist(artistData)
+    console.log(artistData[0])
 
     //Récupère une liste d'albums par rapport à l'artiste [0]
     var returnedAlbums = await fetch(
@@ -99,87 +99,95 @@ function App() {
       })
   }
 
+
   return (
-    <div style={{ height: '75vh' }}>
-      <Container>
-        <InputGroup className="mb-3" size="lg">
-          <FormControl
-            placeholder="Chercher un album"
-            type="input"
-            onKeyUp={event => {
-              if (event.key === 'Enter') {
-                search()
-              }
-            }}
-            onChange={event => setSearchInput(event.target.value)}
-          />
-          <Button onClick={search}>Rechercher</Button>
-        </InputGroup>
-      </Container>
-      <Container>
-        <h4>Artistes</h4>
-        <Row className="mx-5 row row-cols-6">
-          {artist &&
+    <div>
+      {/* Recherche un artiste par le nom */}
+      <div>
+        <input placeholder='Chercher un album' type="input" onKeyUp={event => {
+                if (event.key === 'Enter') {
+                  search()
+                }
+              }} onChange={event => setSearchInput(event.target.value)}></input>
+              <button onClick={search}>Rechercher</button>
+      </div>
+      <div>
+        {artist &&
             artist.length > 0 &&
             artist.slice(0, 6).map((singleArtist, index) => (
-              <Card key={index}>
-                <CardBody>
-                  <CardImg
-                    style={{ width: '2em' }}
-                    src={
-                      singleArtist.images.length > 0
+              <div key={index}>
+              <a>{singleArtist.name}</a>
+              <img style={{ width: '2em' }} src={singleArtist.images.length > 0
                         ? singleArtist.images[0].url
-                        : 'URL_PAR_DEFAUT_SI_AUCUNE_IMAGE'
-                    }
-                  />
-                  <CardTitle style={{ fontSize: '15px' }}>
-                    {singleArtist.name}
-                  </CardTitle>
-                  <CardText style={{ fontSize: '15px' }}>
-                    {singleArtist.followers.total ??
-                      'Nombre de followers non disponible'}
-                  </CardText>
-                </CardBody>
-              </Card>
-            ))}
-        </Row>
-        <h4>Albums du premier Artiste</h4>
-        <Row className="mx-2 row row-cols-6">
-          {albums.slice(0, 6).map((album, i) => {
-            return (
-              <Card key={i}>
-                <Card.Img style={{ width: '2em' }} src={album.images[0].url} />
-                <CardBody>
-                  <CardTitle style={{ fontSize: '15px' }}>
-                    {album.name}
-                  </CardTitle>
-                  <CardText style={{ fontSize: '15px' }}>
-                    Sortie le: {album.release_date}
-                  </CardText>
-                  <CardSubtitle style={{ fontSize: '15px' }}>
-                    Tracks: {album.total_tracks}
-                  </CardSubtitle>
-                </CardBody>
-              </Card>
-            )
-          })}
-        </Row>
-        <h4>Chansons du premier albums</h4>
-        <Row className="mx-2 row row-cols-6">
-          {songs.slice(0, 6).map((song, i) => {
-            return (
-              <Card key={i}>
-                <CardBody>
-                  <CardTitle style={{ fontSize: '15px' }}>
-                    {song.name}
-                  </CardTitle>
-                </CardBody>
-              </Card>
-            )
-          })}
-        </Row>
-      </Container>
+                        : 'URL_PAR_DEFAUT_SI_AUCUNE_IMAGE'}></img>
+              </div>
+        ))}
+      </div>
     </div>
+  //     <Container>
+  //       <h4>Artistes</h4>
+  //       <Row className="mx-5 row row-cols-6">
+  //         {artist &&
+  //           artist.length > 0 &&
+  //           artist.slice(0, 6).map((singleArtist, index) => (
+  //             <Card key={index}>
+  //               <CardBody>
+  //                 <CardImg
+  //                   style={{ width: '2em' }}
+  //                   src={
+  //                     singleArtist.images.length > 0
+  //                       ? singleArtist.images[0].url
+  //                       : 'URL_PAR_DEFAUT_SI_AUCUNE_IMAGE'
+  //                   }
+  //                 />
+  //                 <CardTitle style={{ fontSize: '15px' }}>
+  //                   {singleArtist.name}
+  //                 </CardTitle>
+  //                 <CardText style={{ fontSize: '15px' }}>
+  //                   {singleArtist.followers.total ??
+  //                     'Nombre de followers non disponible'}
+  //                 </CardText>
+  //               </CardBody>
+  //             </Card>
+  //           ))}
+  //       </Row>
+  //       <h4>Albums du premier Artiste</h4>
+  //       <Row className="mx-2 row row-cols-6">
+  //         {albums.slice(0, 6).map((album, i) => {
+  //           return (
+  //             <Card key={i}>
+  //               <Card.Img style={{ width: '2em' }} src={album.images[0].url} />
+  //               <CardBody>
+  //                 <CardTitle style={{ fontSize: '15px' }}>
+  //                   {album.name}
+  //                 </CardTitle>
+  //                 <CardText style={{ fontSize: '15px' }}>
+  //                   Sortie le: {album.release_date}
+  //                 </CardText>
+  //                 <CardSubtitle style={{ fontSize: '15px' }}>
+  //                   Tracks: {album.total_tracks}
+  //                 </CardSubtitle>
+  //               </CardBody>
+  //             </Card>
+  //           )
+  //         })}
+  //       </Row>
+  //       <h4>Chansons du premier albums</h4>
+  //       <Row className="mx-2 row row-cols-6">
+  //         {songs.slice(0, 6).map((song, i) => {
+  //           return (
+  //             <Card key={i}>
+  //               <CardBody>
+  //                 <CardTitle style={{ fontSize: '15px' }}>
+  //                   {song.name}
+  //                 </CardTitle>
+  //               </CardBody>
+  //             </Card>
+  //           )
+  //         })}
+  //       </Row>
+  //     </Container>
+  //   </div>
   )
 }
 
