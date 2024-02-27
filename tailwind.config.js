b422d97e3fd970a0b532718704f6f4eee1b128cf
sohkey <sohkey.fetch@gmail.com>
2024-02-27 10:25:59 +0100

    update: google & spotify login

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        neons: "url('./src/assets/BG.png')",
+      },
+    },
   },
   plugins: [],
 }
