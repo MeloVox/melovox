d0d806f50c0ff5da810a696c57b9500f172e9960
AmauryRDV <amaury.richarddevesvrotte@gmail.com>
2024-03-19 10:55:02 +0100

    import layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login.jsx'
+import Layout from './components/Layout.jsx'
 import Test from './components/Test.jsx'
 import Home from './components/Home.jsx'
 import Notfound from './components/404.jsx'
@@ -17,20 +18,22 @@ function App() {
     <div className="h-screen w-full flex flex-col justify-center items-center bgcolor">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Test />}> */}
-          <Route path="/test" element={<Test />} />
-          <Route path="/Arrow" element={<SwiperArrow />} />
-          <Route path="/Infinite" element={<SwiperInfinite />} />
-          <Route index element={<Background />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/callback" element={<Callback />} />
-          <Route path="/artist/:artistId" element={<Artist />} />
-          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/test" element={<Test />} />
+            <Route path="/Arrow" element={<SwiperArrow />} />
+            <Route path="/Infinite" element={<SwiperInfinite />} />
+            <Route index element={<Background />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/callback" element={<Callback />} />
+            <Route path="/artist/:artistId" element={<Artist />} />
+            <Route path="/about" element={<About />} />
 
-          <Route path="*" element={<Notfound />} />
-          {/* </Route> */}
+            <Route path="*" element={<Notfound />} />
+            {/* </Route> */}
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
