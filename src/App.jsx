9fac52422998774b8bd792222f51c4a8c080b055
NSEREIR <nacim.sereir@ynov.com>
2024-03-19 09:14:52 +0100

    new swipers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './components/Login.jsx'
-import Layout from './components/Layout.jsx'
+import Test from './components/Test.jsx'
 import Home from './components/Home.jsx'
 import Notfound from './components/404.jsx'
 import Register from './components/Register.jsx'
@@ -9,22 +9,26 @@ import Artist from './components/Artist.jsx'
 import Callback from './components/Callback.jsx'
 import About from './components/About.jsx'
 import Background from './components/Background.jsx'
+import SwiperArrow from './components/SwiperArrow.jsx'
+import SwiperInfinite from './components/SwiperInfinite.jsx'
 
 function App() {
   return (
     <div className="h-screen w-full flex flex-col justify-center items-center bgcolor">
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Layout />}> */}
-          <Route index element={<Background />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/callback" element={<Callback />} />
-          <Route path="/artist/:artistId" element={<Artist />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/" element={<Test />}> */}
+            <Route path="/Arrow" element={<SwiperArrow />} />
+            <Route path="/Infinite" element={<SwiperInfinite />} />
+            <Route index element={<Background />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/callback" element={<Callback />} />
+            <Route path="/artist/:artistId" element={<Artist />} />
+            <Route path="/about" element={<About />} />
 
-          <Route path="*" element={<Notfound />} />
+            <Route path="*" element={<Notfound />} />
           {/* </Route> */}
         </Routes>
       </BrowserRouter>
