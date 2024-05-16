04717029600ad68640c860b9190b033cdf0764cc
nacimiento <nsereir.virtualbuilding.com>
2024-05-16 16:55:17 +0200

    g

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,28 @@
+import { Link } from 'react-router-dom'
+
+const Footer = () => {
+  return (
+    <div className="w-full mt-8 bg-slate-900 text-gray-300 py-y px-2 bottom-0 sticky">
+      <div className="w-full mx-auto">
+        <ul className="flex flex-col md:flex-row md:justify-center">
+          <li className="py-2 px-4 text-center md:text-left">
+            <Link to="/about">A Propos</Link>
+          </li>
+          <li className="py-2 px-4 text-center md:text-left">
+            <Link to="/contact">Nous contacter</Link>
+          </li>
+          <li className="py-2 px-4 text-center md:text-left">
+            <Link to="/policy">Politique de confidentialité</Link>
+          </li>
+          <li className="py-2 px-4 text-center md:text-left">
+            <Link to="/tos">Conditions d'utilisation</Link>
+          </li>
+        </ul>
+      </div>
+
+      <p className="text-center py-4">2024 Melovox, Tous droits réservés</p>
+    </div>
+  )
+}
+
+export default Footer
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,32 +1,35 @@
 import Presentation from '../../../src/assets/presentation.png'
 import Background from '../../components/Background/Background'
 
-function About() {
+const About = () => {
   return (
     <>
       <Background />
-      <div className="containerPresentation">
-        <div className="titlePresentation">Qui sommes-nous ?</div>
-        <div className="generalPresentation">
-          <div className="textPresentation">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eu
-            congue augue. Nunc gravida nunc nec purus volutpat, sit amet
-            ullamcorper tortor pellentesque. Vivamus non dolor ante. Nam tempor
-            ut est nec facilisis. In mollis urna tincidunt nunc feugiat, sed
-            tempor nisl bibendum. Maecenas lacinia risus et tellus tempus, id
-            porttitor eros laoreet. Vestibulum id sem id arcu sodales vehicula.
-            Integer elementum lacinia orci non tincidunt. Etiam fringilla, urna
-            semper commodo ornare, nunc metus tempus mi, ac auctor nibh ipsum
-            vel nisi. Donec non condimentum nulla. Suspendisse sed leo a erat
-            auctor placerat. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Cras a erat ut leo sollicitudin sollicitudin interdum at dui.
-            Nulla eu auctor est. Fusce a rutrum risus. Maecenas commodo nulla et
-            lacus elementum porttitor.
+
+      <div
+        className="flex justify-center items-center w-screen h-screen"
+        style={{ border: '2px solid red' }}
+      >
+        <div className="bg-white bg-opacity-20 rounded-lg shadow-md p-6 text-white md:mt-4">
+          <h1 className="text-3xl font-bold mb-8">Qui sommes-nous ?</h1>
+          <div className="flex flex-col md:flex-row">
+            <div className="md:w-1/2 md:pr-4 mb-4 md:mb-0">
+              <h2 className="text-xl font-semibold">Présentation</h2>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
           </div>
         </div>
-      </div>
-      <div className="imagePresentation">
-        <img src={Presentation} className="imgPres" />
+        <img
+          src={Presentation}
+          className="w-15 h-15 absolute bottom-0 right-0 md:w-15 md:h15 md:flex md:bottom-0"
+          alt="Presentation"
+        />
       </div>
     </>
   )
