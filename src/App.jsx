d8b0060eb9850c3a61c87b9e230fee2ceefccc72
nacimiento <nsereir.virtualbuilding.com>
2024-05-16 11:42:37 +0200

    rework header de merde

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HeaderFooter from './components/HeaderFooter/HeaderFooter'
 import Callback from './pages/Callback'
 import Login from './pages/Login'
 import Test from './pages/Test'
@@ -11,28 +10,28 @@ import About from './pages/About/About'
 import Genre from './pages/Genre'
 import Album from './pages/Album'
 import TestComments from './pages/TestComments'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
-    <div className="h-screen w-full flex flex-col justify-center items-center bgcolor">
+    <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<HeaderFooter />}>
-            <Route path="/test" element={<Test />} />
-            <Route path="/testcomments" element={<TestComments />} />
-            <Route path="/genre" element={<Genre />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/callback" element={<Callback />} />
-            <Route path="/artist/:artistId" element={<Artist />} />
-            <Route path="/album/:albumId" element={<Album />} />
-            <Route path="/about" element={<About />} />
-            <Route path="*" element={<Notfound />} />
-          </Route>
+          <Route path="/navbar" element={<Navbar />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/testcomments" element={<TestComments />} />
+          <Route path="/genre" element={<Genre />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/callback" element={<Callback />} />
+          <Route path="/artist/:artistId" element={<Artist />} />
+          <Route path="/album/:albumId" element={<Album />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<Notfound />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </>
   )
 }
 
